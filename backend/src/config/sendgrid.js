a51faf01a7ee9b167da113e0ae2d42724ae776bd
Dhruvermafz <vermadhruv09112002@gmail.com>
2026-05-03 11:01:07 +0530

    add docker + github actions deployment

--- backend/src/config/sendgrid.js
+++ backend/src/config/sendgrid.js
@@ -1,5 +1,6 @@
-import sgMail from '@sendgrid/mail';
+// src/config/sendgrid.js
+const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default sgMail;
+module.exports = sgMail;
